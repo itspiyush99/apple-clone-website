43949d98e4c19a937c56496e59bb572b258367fe
itspiyush99 <121559077+itspiyush99@users.noreply.github.com>
2025-09-04 22:56:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,3 @@
-// import { sentryVitePlugin } from "@sentry/vite-plugin";
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   // base: command === 'serve' ? '/' : '/apple_ui/', 
-//   plugins: [react(), sentryVitePlugin({
-//     org: "jsm-x9",
-//     project: "javascript-react"
-//   })],
-
-//   build: {
-//     sourcemap: true
-//   }
-// })
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
@@ -21,14 +5,12 @@ import { sentryVitePlugin } from '@sentry/vite-plugin';
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
-    base: command === 'serve' ? '/' : '/Apple-website/',  // Use '/' for development, '/apple_ui/' for production
+    base: command === 'serve' ? '/' : '/apple-clone-website/',
     plugins: [
       react(),
       sentryVitePlugin({
         org: 'jsm-x9',
         project: 'javascript-react',
-        // Optionally include the authToken if needed for Sentry
-        // authToken: 'YOUR_SENTRY_AUTH_TOKEN',
       }),
     ],
     build: {
@@ -36,3 +18,4 @@ export default defineConfig(({ command }) => {
     },
   };
 });
+
